01b1302d608faf24cfb3dbe7314bbdd6e98f9a48
Dreimond <slavik161299@gmail.com>
2024-12-10 04:41:02 +0200

    develope2

--- config/env_config.py
+++ config/env_config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
-db_path = os.getenv("DATABASE_URL")
-#DATABASE_URL=os.getenv("DATABASE_URL")
+db_path = os.getenv("DATABASE_PATH")
+DATABASE_URL=os.getenv("DATABASE_URL")
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config'))
